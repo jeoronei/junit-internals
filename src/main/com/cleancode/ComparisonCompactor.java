2904d8621086ba03fbbb0bf0df5492d9b8ef1436
Jeoronei Ribinski <jeoronei.ribinski@contabilizei.com.br>
2020-06-10 18:17:09 -0300

    Refactoring ComparisonCompactor

--- src/main/com/cleancode/ComparisonCompactor.java
+++ src/main/com/cleancode/ComparisonCompactor.java
@@ -1,61 +1,3 @@
-/* ========================================================================
- * JCommon : a free general purpose class library for the Java(tm) platform
- * ========================================================================
- *
- * (C) Copyright 2000-2006, by Object Refinery Limited and Contributors.
- *
- * Project Info:  http://www.jfree.org/jcommon/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc.
- * in the United States and other countries.]
- *
- * ---------------
- * SerialDate.java
- * ---------------
- * (C) Copyright 2001-2006, by Object Refinery Limited.
- *
- * Original Author:  David Gilbert (for Object Refinery Limited);
- * Contributor(s):   -;
- *
- * $Id: SerialDate.java,v 1.9 2011/10/17 20:08:22 mungady Exp $
- *
- * Changes (from 11-Oct-2001)
- * --------------------------
- * 11-Oct-2001 : Re-organised the class and moved it to new package
- *               com.jrefinery.date (DG);
- * 05-Nov-2001 : Added a getDescription() method, and eliminated NotableDate
- *               class (DG);
- * 12-Nov-2001 : IBD requires setDescription() method, now that NotableDate
- *               class is gone (DG);  Changed getPreviousDayOfWeek(),
- *               getFollowingDayOfWeek() and getNearestDayOfWeek() to correct
- *               bugs (DG);
- * 05-Dec-2001 : Fixed bug in SpreadsheetDate class (DG);
- * 29-May-2002 : Moved the month constants into a separate interface
- *               (MonthConstants) (DG);
- * 27-Aug-2002 : Fixed bug in addMonths() method, thanks to N???levka Petr (DG);
- * 03-Oct-2002 : Fixed errors reported by Checkstyle (DG);
- * 13-Mar-2003 : Implemented Serializable (DG);
- * 29-May-2003 : Fixed bug in addMonths method (DG);
- * 04-Sep-2003 : Implemented Comparable.  Updated the isInRange javadocs (DG);
- * 05-Jan-2005 : Fixed bug in addYears() method (1096282) (DG);
- *
- */
-
 package com.cleancode;
 import static junit.framework.Assert.format;
 
@@ -63,77 +5,92 @@ public class ComparisonCompactor {
     private static final String ELLIPSIS = "...";
     private static final String DELTA_END = "]";
     private static final String DELTA_START = "[";
-    private int fContextLength;
-    private String fExpected;
-    private String fActual;
-    private int fPrefix;
-    private int fSuffix;
+    private int contextLength;
+    private String expected;
+    private String actual;
+    private int prefixLength;
+    private int suffixLength;
 
-    public ComparisonCompactor(int contextLength,
-                               String expected,
-                               String actual) {
-        fContextLength = contextLength;
-        fExpected = expected;
-        fActual = actual;
+    public ComparisonCompactor(
+            int contextLength, String expected, String actual
+    ) {
+        this.contextLength = contextLength;
+        this.expected = expected;
+        this.actual = actual;
     }
-
-    public String compact(String message) {
-        if (fExpected == null || fActual == null || areStringsEqual())
-            return format(message, fExpected, fActual);
+    public String formatCompactedComparison(String message) {
+        String compactExpected = expected;
+        String compactActual = actual;
+        if (shouldBeCompacted()) {
+            findCommonPrefixAndSuffix();
+            compactExpected = compact(expected);
+            compactActual = compact(actual);
+        }
+        return format(message, compactExpected, compactActual);
+    }
+    private boolean shouldBeCompacted() {
+        return !shouldNotBeCompacted();
+    }
+    private boolean shouldNotBeCompacted() {
+        return expected == null ||
+                actual == null ||
+                expected.equals(actual);
+    }
+    private void findCommonPrefixAndSuffix() {
         findCommonPrefix();
-        findCommonSuffix();
-        String expected = compactString(fExpected);
-        String actual = compactString(fActual);
-        return format(message, expected, actual);
+        suffixLength = 0;
+        for (; !suffixOverlapsPrefix(); suffixLength++) {
+            if (charFromEnd(expected, suffixLength) !=
+                    charFromEnd(actual, suffixLength)
+            )
+                break;
+        }
     }
-
-    private String compactString(String source) {
-        String result = DELTA_START +
-                source.substring(fPrefix, source.length() -
-                        fSuffix + 1) + DELTA_END;
-        if (fPrefix > 0)
-            result = computeCommonPrefix() + result;
-        if (fSuffix > 0)
-            result = result + computeCommonSuffix();
-        return result;
+    private char charFromEnd(String s, int i) {
+        return s.charAt(s.length() - i - 1);
     }
-
+    private boolean suffixOverlapsPrefix() {
+        return actual.length() - suffixLength <= prefixLength ||
+                expected.length() - suffixLength <= prefixLength;
+    }
     private void findCommonPrefix() {
-        fPrefix = 0;
-        int end = Math.min(fExpected.length(), fActual.length());
-        for (; fPrefix < end; fPrefix++) {
-            if (fExpected.charAt(fPrefix) != fActual.charAt(fPrefix))
+        prefixLength = 0;
+        int end = Math.min(expected.length(), actual.length());
+        for (; prefixLength < end; prefixLength++)
+            if (expected.charAt(prefixLength) != actual.charAt(prefixLength))
                 break;
-        }
     }
-
-    private void findCommonSuffix() {
-        int expectedSuffix = fExpected.length() - 1;
-        int actualSuffix = fActual.length() - 1;
-        for (;
-             actualSuffix >= fPrefix && expectedSuffix >= fPrefix;
-             actualSuffix--, expectedSuffix--) {
-            if (fExpected.charAt(expectedSuffix) != fActual.charAt(actualSuffix))
-                break;
-        }
-        fSuffix = fExpected.length() - expectedSuffix;
+    private String compact(String s) {
+        return new StringBuilder()
+                .append(startingEllipsis())
+                .append(startingContext())
+                .append(DELTA_START)
+                .append(delta(s))
+                .append(DELTA_END)
+                .append(endingContext())
+                .append(endingEllipsis())
+                .toString();
     }
-
-    private String computeCommonPrefix() {
-        return (fPrefix > fContextLength ? ELLIPSIS : "") +
-                fExpected.substring(Math.max(0, fPrefix - fContextLength),
-                        fPrefix);
+    private String startingEllipsis() {
+        return prefixLength > contextLength ? ELLIPSIS : "";
     }
-
-    private String computeCommonSuffix() {
-        int end = Math.min(fExpected.length() - fSuffix + 1 + fContextLength,
-                fExpected.length());
-        return fExpected.substring(fExpected.length() - fSuffix + 1, end) +
-                (fExpected.length() - fSuffix + 1 < fExpected.length() -
-                        fContextLength ? ELLIPSIS : "");
+    private String startingContext() {
+        int contextStart = Math.max(0, prefixLength - contextLength);
+        int contextEnd = prefixLength;
+        return expected.substring(contextStart, contextEnd);
     }
-
-    private boolean areStringsEqual() {
-        return fExpected.equals(fActual);
+    private String delta(String s) {
+        int deltaStart = prefixLength;
+        int deltaEnd = s.length() - suffixLength;
+        return s.substring(deltaStart, deltaEnd);
+    }
+    private String endingContext() {
+        int contextStart = expected.length() - suffixLength;
+        int contextEnd =
+                Math.min(contextStart + contextLength, expected.length());
+        return expected.substring(contextStart, contextEnd);
+    }
+    private String endingEllipsis() {
+        return (suffixLength > contextLength ? ELLIPSIS : "");
     }
 }
